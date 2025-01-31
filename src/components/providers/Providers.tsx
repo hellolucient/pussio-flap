6099ef2be69355b024f2c6a43e8b8dd7f4984722
hellolucient <hellolucient@gmail.com>
2025-01-31 15:04:23 +0700

    Remove TanStack Query and simplify Providers component

--- src/components/providers/Providers.tsx
+++ src/components/providers/Providers.tsx
@@ -1,16 +1,11 @@
 "use client"
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { FlapsProvider } from '@/contexts/FlapsContext'
-
-const queryClient = new QueryClient()
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <FlapsProvider>
-        {children}
-      </FlapsProvider>
-    </QueryClientProvider>
+    <FlapsProvider>
+      {children}
+    </FlapsProvider>
   )
 } 
